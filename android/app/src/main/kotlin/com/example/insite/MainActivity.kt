feacbb239d64a0b7c30efdf3e2bcce8fb7eab7b9
sabarinathan <nathans441@gmail.com>
2021-03-29 09:33:32 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/insite/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.insite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const Color mercury = Color(0xFFE5E5E5);
