b45db2e822c1464ee2e9fb74121f84dc6fa5bde9
SamsonChris71 <samsonchris71@gmail.com>
2021-04-07 13:21:23 +0530

    Added Custom Widget: Notes

---
+++ lib/widgets/smart_widgets/notes.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:insite/widgets/dumb_widgets/insite_button.dart';
+
+class Notes extends StatelessWidget {
+  const Notes({
+    Key key,
+    @required this.controller,
+    @required this.onTap,
+  }) : super(key: key);
+
+  final TextEditingController controller;
+  final Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final kTextFieldBorder = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.transparent,
+      ),
+      borderRadius: BorderRadius.all(
+        const Radius.circular(10.0),
+      ),
+    );
+
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.9,
+      height: MediaQuery.of(context).size.height * 0.2,
+      decoration: new BoxDecoration(
+        color: Color(0xFF303238),
+        border: Border.all(color: Colors.black, width: 0.0),
+        borderRadius: BorderRadius.all(Radius.circular(16)),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Icon(
+                  Icons.keyboard_arrow_down,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'NOTES',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Stack(
+                alignment: AlignmentDirectional.bottomEnd,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: Material(
+                      elevation: 5.0,
+                      borderRadius: BorderRadius.all(
+                        const Radius.circular(30.0),
+                      ),
+                      child: TextFormField(
+                        controller: controller,
+                        keyboardType: TextInputType.text,
+                        maxLines: null,
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                        autofocus: false,
+                        decoration: InputDecoration(
+                          focusedBorder: kTextFieldBorder,
+                          border: kTextFieldBorder,
+                          enabledBorder: kTextFieldBorder,
+                          disabledBorder: kTextFieldBorder,
+                          hintText: 'Enter Notes here',
+                          isCollapsed: true,
+                          fillColor: Colors.white,
+                          filled: true,
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical:
+                                  MediaQuery.of(context).size.height * 0.045,
+                              horizontal:
+                                  MediaQuery.of(context).size.width * 0.02),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: InsiteButton(
+                      title: 'Add',
+                      width: 90,
+                      height: 40,
+                      onTap: onTap,
+                      bgColor: Color(0xFFF37021),
+                      textColor: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
