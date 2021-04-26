f2e21d1cb5077d18714c935e130268beb7eada82
sabarinathan <nathans441@gmail.com>
2021-04-26 11:05:14 +0530

    integrated location module from testroute to asset detail page

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       navigatorKey: locator<NavigationService>().navigatorKey,
       onGenerateRoute: router.Router.generateRoute,
-      initialRoute: utilRouterViewRoute,
+      initialRoute: splashViewRoute,
       theme: ThemeData(
           backgroundColor: cod_grey,
           fontFamily: 'Roboto',
