7a78d84fdc0eac035c4e0b4a68be30f36e5c1cf9
SamsonChris71 <samsonchris71@gmail.com>
2021-06-10 10:14:42 +0530

    Fixed: Removed unwanted routes

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -3,7 +3,8 @@
 import 'package:flutter/material.dart';
 import 'package:insite/core/router_constants.dart';
 import 'package:insite/views/asset_operation/asset_list_view.dart';
-
+import 'package:insite/views/dashboard/dashboard_view.dart' as view8;
+import 'package:insite/views/detail/asset_detail_view.dart' as view9;
 import 'package:insite/views/splash/splash_view.dart' as view0;
 import 'package:insite/views/customer_selection/customer_selection_view.dart'
     as view1;
@@ -12,13 +13,9 @@ import 'package:insite/views/fleet/fleet_view.dart' as view3;
 import 'package:insite/views/login/login_view.dart' as view4;
 import 'package:insite/views/logout/logout_view.dart' as view5;
 import 'package:insite/views/global_search/global_search_view.dart' as view6;
-import 'package:insite/views/dashboard/dashboard_view.dart' as view7;
-import 'package:insite/views/detail/asset_detail_view.dart' as view9;
+import 'package:insite/views/test_router/test_router_view.dart' as view7;
+import 'package:insite/views/utilization/utilization_view.dart' as view11;
 import 'package:insite/views/location/location_view.dart' as view10;
-import 'package:insite/views/utilization/tabs/graph_view/utilization_graph_view.dart'
-    as view12;
-import 'package:insite/views/utilization/tabs/list/utilization_list_view.dart'
-    as view11;
 
 class Router {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -37,19 +34,22 @@ class Router {
         return MaterialPageRoute(builder: (_) => view5.LogoutView());
       case globalSearchViewRoute:
         return MaterialPageRoute(builder: (_) => view6.GlobalSearchView());
+      case testRouterViewRoute:
+        return MaterialPageRoute(builder: (_) => view7.TestRouterView());
       case dashboardViewRoute:
-        return MaterialPageRoute(builder: (_) => view7.DashboardView());
-
-      case assetDetailViewRoute:
-        return MaterialPageRoute(builder: (_) => view9.AssetDetailView());
+        return MaterialPageRoute(builder: (_) => view8.DashboardView());
       case locationViewRoute:
         return MaterialPageRoute(builder: (_) => view10.LocationView());
-      case utilizationListViewRoute:
-        return MaterialPageRoute(builder: (_) => view11.UtilizationListView());
-      case utilizationGraphViewRoute:
-        return MaterialPageRoute(builder: (_) => view12.UtilizationGraphView());
+      case assetDetailViewRoute:
+        var fleetArgs = settings.arguments as view9.DetailArguments;
+        return MaterialPageRoute(
+            builder: (_) => view9.AssetDetailView(
+                  fleet: fleetArgs.fleet,
+                ));
       case assetViewRoute:
         return MaterialPageRoute(builder: (_) => AssetListView());
+      case utilRouterViewRoute:
+        return MaterialPageRoute(builder: (_) => view11.UtilLizationView());
 
       default:
         return MaterialPageRoute(
