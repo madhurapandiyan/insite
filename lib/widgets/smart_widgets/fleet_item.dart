76b20832e9909d44bfa2b13c9da93f6b3cf475fb
siva prajith <siva@fidisys.com>
2021-06-25 20:19:52 +0530

    method to be change in util class

--- lib/widgets/smart_widgets/fleet_item.dart
+++ lib/widgets/smart_widgets/fleet_item.dart
@@ -55,7 +55,7 @@ class FleetListItem extends StatelessWidget {
                       children: [
                         InsiteTableRowWithImage(
                           title: fleet.dealerName,
-                          path: imageData(fleet.model),
+                          path: Utils().imageData(fleet.model),
                         ),
                         InsiteTableRowItem(
                           title: "Last Known Status",
@@ -163,23 +163,5 @@ class FleetListItem extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  imageData(String model) {
-    if (model.contains("SHINRAI")) {
-      return "assets/images/shinrai.png";
-    } else if (model.contains("EX130")) {
-      return "assets/images/EX130.png";
-    } else if (model.contains("EX210")) {
-      return "assets/images/EX210.png";
-    } else if (model.contains("EX210LC")) {
-      return "assets/images/EX210LC.png";
-    } else if (model.contains("TH86")) {
-      return "assets/images/TH86.png";
-    } else if (model.contains("TL340H")) {
-      return "assets/images/TL340H.png";
-    } else {
-      return "assets/images/EX210.png";
-    }
   }
 }
