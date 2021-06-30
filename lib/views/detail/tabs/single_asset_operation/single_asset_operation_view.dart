db6dacede6bf5f686a5eccc808a3e94dc2bfd0ec
SamsonChris71 <samsonchris71@gmail.com>
2021-06-30 11:30:32 +0530

    Finished: Single asset operation chart

--- lib/views/detail/tabs/single_asset_operation/single_asset_operation_view.dart
+++ lib/views/detail/tabs/single_asset_operation/single_asset_operation_view.dart
@@ -264,7 +264,7 @@ class _SingleAssetOperationViewState extends State<SingleAssetOperationView> {
                                           child: SvgPicture.asset(
                                             "assets/images/minus.svg",
                                           ),
-                                        )),
+                                        ),),
                                   ],
                                 ),
                               ),
