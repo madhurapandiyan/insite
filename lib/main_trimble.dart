512d223d8291e64fbf28ca45db0207c369dc90c5
Sabarinathan <nathans441@gmail.com>
2021-10-11 01:53:43 +0530

    theme changed for newly added flavor trimble

--- lib/main_trimble.dart
+++ lib/main_trimble.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
-import 'package:insite/core/flavor/flavor.dart';
 import 'package:insite/core/models/filter_data.dart';
-import 'package:insite/core/router_constants.dart';
+import 'package:insite/core/router_constants_india_stack.dart';
 import 'package:insite/theme/colors.dart';
 import 'package:load/load.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'core/flavor/flavor.dart';
 import 'core/locator.dart';
 import 'core/models/db/asset_count_data.dart';
-import 'core/router.dart' as router;
+import 'core/router_india_stack.dart' as router;
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
@@ -20,7 +20,9 @@ void main() async {
   Hive.registerAdapter<CountData>(CountDataAdapter());
   Hive.registerAdapter<FilterSubType>(FilterSubTypeAdapter());
   AppConfig(
-      baseUrl: "https://unifiedfleet.myvisionlink.com", flavor: "visionlink");
+      baseUrl: "https://cloud.api.trimble.com/CTSPulseIndiastg",
+      flavor: "indiastack");
+
   await LocatorInjector.setUpLocator();
   runApp(MyApp());
 }
@@ -29,23 +31,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LoadingProvider(
-      themeData: LoadingThemeData(
-        loadingBackgroundColor: Colors.white,
-        loadingPadding: EdgeInsets.all(24),
-        loadingSize: Size(80, 80),
-        borderRadius: BorderRadius.circular(4),
-      ),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        navigatorKey: locator<NavigationService>().navigatorKey,
-        onGenerateRoute: router.Router.generateRoute,
-        initialRoute: splashViewRoute,
-        theme: ThemeData(
-            backgroundColor: cod_grey,
-            fontFamily: 'Roboto',
-            appBarTheme: AppBarTheme(backgroundColor: Colors.white),
-            accentColor: Colors.white),
-      ),
-    );
+        themeData: LoadingThemeData(
+          loadingBackgroundColor: Colors.white,
+          backgroundColor: tango,
+          loadingPadding: EdgeInsets.all(24),
+          loadingSize: Size(80, 80),
+          borderRadius: BorderRadius.circular(4),
+        ),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          navigatorKey: locator<NavigationService>().navigatorKey,
+          onGenerateRoute: router.Router.generateRoute,
+          initialRoute: indiaStackSplashViewRoute,
+          theme: indiaStackBlueWhite,
+        ));
   }
 }
